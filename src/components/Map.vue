5504d9cff0985dae04efbfbad423d042a78f65eb
rsabaric <sabaricr@seattleu.edu>
2018-11-30 23:29:40 -0800

    interactive chart

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -16,9 +16,16 @@
       :data="mapData"
       :options="mapOptions"
       :events="chartEvents"
-      v-on:select="console.log('click')"
+      ref="gChart"
     />
-    <button v-on:click="refresh">Get Current Temperatures</button>    
+    <button v-on:click="refresh">Get Current Temperatures</button>
+    <p>Your Current Selection: {{selectionCity}}</p>
+    <img v-bind:src="companion[0].link" class="thumbnail"></img>
+    <p v-if="selectionTemp<40">Brrrr, it's only It's {{selectionTemp}} degrees in {{selectionCity}}, you better pack that parka!</p>
+    <p v-else-if="selectionTemp<60">It's {{selectionTemp}} degrees in {{selectionCity}}, you might want to pack a sweater!</p>
+    <p v-else-if="selectionTemp<80">It's {{selectionTemp}} degrees in {{selectionCity}}, that sounds nice! Pack for pleasant weather.</p>
+    <p v-else-if="selectionTemp<100">It's {{selectionTemp}} degrees in {{selectionCity}}, pack some shorts and flip-flops!</p>
+    <p v-else>Yikes, it's {{selectionTemp}} degrees in {{selectionCity}}, pack some shorts and drink plenty of water!</p>  
   </div>
   
   </div>
@@ -49,19 +56,25 @@ export default {
         ['Seattle, WA',      50,    .5],
         ['Honolulu, HI',     80,    .5],
         ['San Jose, Costa Rica',    90,    .5],
-        ['Venice',     60,    .5],
-        ['Cozumel',   80,    .5],
-        ['Cabo San Lucas',     70,    .5],
-        ['Aukland',   50,    .5],
-        ['New York',  50,    .5],
-        ['Spokane', 40,    .5],
-        ['Tokyo',     50,    .5],
-        ['Madrid',  60,    .5]
+        ['Venice, Italy',     60,    .5],
+        ['Cozumel, Mexico',   80,    .5],
+        ['Cabo San Lucas, Mexico',     70,    .5],
+        ['Aukland, New Zealand',   50,    .5],
+        ['New York, NY',  50,    .5],
+        ['Spokane, WA', 40,    .5],
+        ['Tokyo, Japan',     50,    .5],
+        ['Madrid, Spain',  60,    .5]
       ],
+      selectionCity:'',
+      selectionTemp:0,
+      dogMessage: '',
       mapOptions:{displayMode:'markers',colorAxis: {colors: ['#4374e0','#e7711c']}},
       chartEvents: {
         'select': () => {
-          console.log('click')
+          const chartMap = this.$refs.gChart.chartObject;
+          const selection = chartMap.getSelection();
+          this.selectionCity = this.mapData[(selection[0].row+1)][0];
+          this.selectionTemp = this.mapData[(selection[0].row+1)][1].toString();
         }
       }
       
@@ -81,16 +94,16 @@ export default {
         ['Seattle, WA',      this.results.list[0].main.temp,    this.results.list[0].wind.speed],
         ['Honolulu, HI',     this.results.list[1].main.temp,    this.results.list[1].wind.speed],
         ['San Jose, Costa Rica',    this.results.list[2].main.temp,    this.results.list[2].wind.speed],
-        ['Venice',     this.results.list[3].main.temp,    this.results.list[3].wind.speed],
-        ['Cozumel',   this.results.list[4].main.temp,    this.results.list[4].wind.speed],
-        ['Cabo San Lucas',     this.results.list[5].main.temp,    this.results.list[5].wind.speed],
-        ['Aukland',   this.results.list[6].main.temp,    this.results.list[6].wind.speed],
-        ['New York',  this.results.list[7].main.temp,    this.results.list[7].wind.speed],
-        ['Spokane', this.results.list[8].main.temp,    this.results.list[8].wind.speed],
-        ['Tokyo',     this.results.list[9].main.temp,    this.results.list[9].wind.speed],
-        ['Madrid',  this.results.list[10].main.temp,    this.results.list[10].wind.speed]
+        ['Venice, Italy',     this.results.list[3].main.temp,    this.results.list[3].wind.speed],
+        ['Cozumel, Mexixo',   this.results.list[4].main.temp,    this.results.list[4].wind.speed],
+        ['Cabo San Lucas, Mexico',     this.results.list[5].main.temp,    this.results.list[5].wind.speed],
+        ['Aukland, New Zealand',   this.results.list[6].main.temp,    this.results.list[6].wind.speed],
+        ['New York, NY',  this.results.list[7].main.temp,    this.results.list[7].wind.speed],
+        ['Spokane, WA', this.results.list[8].main.temp,    this.results.list[8].wind.speed],
+        ['Tokyo, Japan',     this.results.list[9].main.temp,    this.results.list[9].wind.speed],
+        ['Madrid, Spain',  this.results.list[10].main.temp,    this.results.list[10].wind.speed]
       ]
-        }
+      }
 },
 created () {
       this.companion = this.$ls.get('selectedDog');
@@ -128,5 +141,10 @@ created () {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.thumbnail {
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+    width: 150px;
+}
 </style>
